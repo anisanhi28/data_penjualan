58d347f128b6ee63eba9a6f0aac0ba5e9fb26884
Rizal Fathur Rahman <rizalfathurrahman0123@gmail.com>
2025-08-03 00:48:01 +0700

    handle redirect after PUT,PATCH,or DELETE request

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -57,7 +57,7 @@ class CategoryController extends Controller
             'nama' => $request->nama,
         ]);
 
-        return redirect()->route('categories.index')->with('success', 'Kategori berhasil diperbarui.');
+        return redirect()->route('categories.index',[],303)->with('success', 'Kategori berhasil diperbarui.');
     }
 
     public function destroy($id)
@@ -65,7 +65,7 @@ class CategoryController extends Controller
         $category = Category::findOrFail($id);
         $category->delete();
 
-        return redirect()->route('categories.index')->with('success', 'Kategori berhasil dihapus.');
+        return redirect()->route('categories.index',[],303)->with('success', 'Kategori berhasil dihapus.');
     }
 
     public function show($id)
